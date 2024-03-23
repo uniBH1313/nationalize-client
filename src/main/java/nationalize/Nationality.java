122d111a3bc22d42b4b27da320c32ee80aa44789
uniBH1313 <huszti.barbara@mailbox.unideb.hu>
2024-03-23 15:28:49 +0100

    refactor: use Lombok in Nationality

--- src/main/java/nationalize/Nationality.java
+++ src/main/java/nationalize/Nationality.java
@@ -1,11 +1,11 @@
 package nationalize;
 
 import com.google.gson.annotations.SerializedName;
-import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
-import org.apache.commons.lang3.builder.ToStringStyle;
+
 
 import java.util.List;
 
+@lombok.Data
 public class Nationality {
 
     private long count;
@@ -20,10 +20,7 @@ public class Nationality {
         this.countries = countries;
     }
 
-    @Override
-    public String toString() {
-        return ReflectionToStringBuilder.toString(this, ToStringStyle.NO_CLASS_NAME_STYLE);
-    }
+
 
     public long getCount() {
         return count;
@@ -49,6 +46,7 @@ public class Nationality {
         this.countries = countries;
     }
 
+    @lombok.Data
     public static class Country {
 
         private String countryId;
@@ -59,10 +57,7 @@ public class Nationality {
             this.probability = probability;
         }
 
-        @Override
-        public String toString() {
-            return ReflectionToStringBuilder.toString(this, ToStringStyle.NO_CLASS_NAME_STYLE);
-        }
+
 
         public String getCountryId() {
             return countryId;
